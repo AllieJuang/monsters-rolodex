ff5b98ee76676ffa887cc4f3d3c0f5ed927ac361
allie <allie@25sprout.com>
2020-02-02 15:16:11 +0800

    Updates

---
+++ precache-manifest.3e49c0012034654b8aca1dd9930c8215.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f8f9e306d329c2b6651af785ddc5eb69",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "0995bd864700bb019ae0",
+    "url": "/monsters-rolodex/static/css/main.f02b3c63.chunk.css"
+  },
+  {
+    "revision": "617e0f92aa616ae4c4f4",
+    "url": "/monsters-rolodex/static/js/2.6fd2d76f.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/monsters-rolodex/static/js/2.6fd2d76f.chunk.js.LICENSE"
+  },
+  {
+    "revision": "0995bd864700bb019ae0",
+    "url": "/monsters-rolodex/static/js/main.dfd73d48.chunk.js"
+  },
+  {
+    "revision": "517fd9319d43d3a57612",
+    "url": "/monsters-rolodex/static/js/runtime-main.ae75c310.js"
+  }
+]);
